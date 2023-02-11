e0acfca3a998236a9bbc10aa589afe8415fd5308
Nour-eddine <92918272+F-nour@users.noreply.github.com>
2023-02-11 08:19:04 +0100

    initialisation angular project

---
+++ src/app/skills/skills.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SkillsRoutingModule } from './skills-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SkillsRoutingModule
+  ]
+})
+export class SkillsModule { }
